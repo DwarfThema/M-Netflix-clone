e1e2be2c98c5e1cc8c54bf32b9b9866e235271b1
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-17 01:39:08 +0900

    gradient background used by useMotionValue&useTransform

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,13 @@ import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 const Box = styled(motion.div)`
@@ -38,13 +28,26 @@ const boxVars = {
 
 function App() {
   const x = useMotionValue(0);
-  const boxscale = useTransform(x, [-800, 0, 800], [2, 1, 0.2]);
+  const scale = useTransform(x, [-800, 800], [2, 0.2]);
+  const rotate = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(36, 238, 0), rgb(0, 238, 226))",
+    ]
+  );
   useEffect(() => {
     x.onChange(() => console.log(x.get()));
   }, [x]);
   return (
-    <Wrapper>
-      <Box style={{ x, scale: boxscale }} drag="x" dragSnapToOrigin></Box>
+    <Wrapper style={{ background: gradient }}>
+      <Box
+        style={{ x, scale, rotateZ: rotate }}
+        drag="x"
+        dragSnapToOrigin
+      ></Box>
     </Wrapper>
   );
 }
