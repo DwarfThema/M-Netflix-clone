efc956840e3146bebfc41c7d2b1d45442e69f283
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-17 01:02:47 +0900

    drag&dragConstraints&Ref&ovwerflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -9,10 +10,21 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: grid;
@@ -22,19 +34,24 @@ const Box = styled(motion.div)`
 const boxVars = {
   hover: { scale: 2, rotateZ: 360 },
   click: { scale: 1, borderRadius: "50%" },
-  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 1 } },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVars}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="click"
-      ></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragConstraints={biggerBoxRef}
+          dragElastic={0.5}
+          variants={boxVars}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
