8161da2d876df8dc8da695e8c05a7938428b5c87
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-17 00:48:21 +0900

    hover&click

--- src/App.tsx
+++ src/App.tsx
@@ -12,66 +12,29 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: white;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: grid;
   grid-template-columns: repeat(2, 1fr);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", bounce: 0.5 } },
-};
-
 const boxVars = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildre: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const cirVars = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    scale: 1,
-    y: 0,
-    opacity: 1,
-  },
+  hover: { scale: 2, rotateZ: 360 },
+  click: { scale: 1, borderRadius: "50%" },
+  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 1 } },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        <Circle variants={cirVars} />
-        <Circle variants={cirVars} />
-        <Circle variants={cirVars} />
-        <Circle variants={cirVars} />
-      </Box>
+      <Box
+        drag
+        variants={boxVars}
+        whileHover="hover"
+        whileDrag="drag"
+        whileTap="click"
+      ></Box>
     </Wrapper>
   );
 }
