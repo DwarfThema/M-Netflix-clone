6297282007e3e673ede412757fe1d2e673416d19
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-17 17:40:23 +0900

    Grid Layout(1)_overLay & Box animation

--- src/App.tsx
+++ src/App.tsx
@@ -17,21 +17,35 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 
+const Grid = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 28px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  border-radius: 50px;
+
+const OverLay = styled(motion.div)`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
@@ -39,12 +53,26 @@ function App() {
   const toggleClicked = () => setClicked((prev) => !prev);
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box>
-        {clicked ? <Circle layoutId="1" style={{ borderRadius: 40 }} /> : null}
-      </Box>
-      <Box>
-        {!clicked ? <Circle layoutId="1" style={{ borderRadius: 0 }} /> : null}
-      </Box>
+      <Grid>
+        <Box layoutId="hello" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {clicked ? (
+          <OverLay
+            initial={{
+              backgroundColor: "rgba(0, 0, 0, 0)",
+            }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box style={{ width: 400, height: 200 }} layoutId="hello" />
+          </OverLay>
+        ) : null}
+      </AnimatePresence>
+      {/* 애니메이트 프레젠스를 쓰면 3개의 옵션을 준다. initial / animate / exit 는 순서대로의 state이다. */}
     </Wrapper>
   );
 }
