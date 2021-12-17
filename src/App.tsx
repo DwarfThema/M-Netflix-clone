5f1b9d3e4acf67f9ff1a8a4dea47b44e2d6b8fc5
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-17 14:37:48 +0900

    AnimatePresence : slider(1)

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
@@ -23,43 +24,55 @@ const Box = styled(motion.div)`
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  top: 300px;
+  position: absolute;
 `;
 
-const boxVariants = {
+const box = {
   initial: {
+    x: 500,
+    opacity: 0,
+    scale: 0,
+  },
+  animate: {
+    x: 0,
     opacity: 1,
     scale: 1,
   },
-  visible: {
-    opcity: 1,
-    scale: 1,
-    rotateZ: 360,
-  },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
-    y: 50,
+    scale: 0,
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 1 : prev + 1));
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        (
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              key={i}
+              variants={box}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>click</button>
     </Wrapper>
   );
 }
