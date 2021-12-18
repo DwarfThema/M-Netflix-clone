2bd1e04d0548fbe3be144105feef8c9c43a80e2f
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-18 23:28:38 +0900

    building Routes & Header foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,19 @@
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
-import { useEffect, useRef } from "react";
-import styled from "styled-components";
-
-const Wrapper = styled(motion.div)`
-  height: 500vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-`;
-
-const boxVars = {
-  hover: { scale: 2, rotateZ: 360 },
-  click: { scale: 1, borderRadius: "50%" },
-};
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
-  const x = useMotionValue(0);
-  const rotate = useTransform(x, [-800, 800], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
-      "linear-gradient(135deg, rgb(36, 238, 0), rgb(0, 238, 226))",
-    ]
-  );
-  /*   useEffect(() => {
-    x.onChange(() => console.log(x.get()));
-  }, [x]); */
-
-  const { scrollY, scrollYProgress } = useViewportScroll();
-  /*   useEffect(() => {
-    scrollY.onChange(() => {
-      console.log(scrollY.get(), scrollYProgress.get());
-    });
-  }, [scrollYProgress, scrollY]); */
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
   return (
-    <Wrapper style={{ background: gradient }}>
-      <Box
-        style={{ x, scale: scale, rotateZ: rotate }}
-        drag="x"
-        dragSnapToOrigin
-      ></Box>
-    </Wrapper>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
